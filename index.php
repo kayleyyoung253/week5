b40f6c8bac252a2b25b32be2abbd009f6a552cd5
kayley.young253 <kayley.young253@gmail.com>
2024-02-01 14:58:16 -0500

    Added f3 templating with arrays

--- index.php
+++ index.php
@@ -10,13 +10,19 @@ require_once ('vendor/autoload.php');
 $f3 = Base::instance();  //instance method
 
 //Define a default route-invoking route method
-//$f3->route('GET /', function() {
-    //echo "Pet Home";
+$f3->route('GET /', function($f3) {
+    //echo "Week 5";
 
+    $f3->set('username', 'jshmo');
+    $f3->set('password', sha1('password01')); //sha1 is encryption
+    $f3->set('title', 'Working with Templates');
+    $f3->set('temp', 67);
+    $f3->set('fruits', array('apple', 'orange', 'banana'));
+    $f3->set('veggies', array('carrot', 'cucumber', 'asparagus'));
     //display a view page
-   // $view = new Template();// template is a class from fat-free
-   // echo $view->render('views/home.html');
-//});
+    $view = new Template();// template is a class from fat-free
+    echo $view->render('views/info.html');
+});
 
 //run Fat-Free
 $f3->run();
